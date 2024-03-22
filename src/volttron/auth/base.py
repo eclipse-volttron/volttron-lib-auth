19b31bf66917d0f1c6a80bb707bca154621e920a
C. Allwardt <3979063+craig8@users.noreply.github.com>
2024-03-22 08:13:51 -0700

    working now

---
+++ src/volttron/auth/base.py
@@ -0,0 +1,9 @@
+# BaseAuthentication class
+class BaseAuthentication:
+
+    def create_authentication_parameters(self):
+        """
+        Used to create an authenticated address
+        based on authentication protocol and message bus.
+        """
+        pass
